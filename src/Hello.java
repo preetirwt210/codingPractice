91d86e55dc960d3a41c4b92dd39e278e8e7ab5a5
Preeti Rawat <preetirawat711@gmail.com>
2021-04-01 18:31:43 +0530

    myFirst commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello World");
+
+        int myFirstNumber=10+6;
+        System.out.println("myFirstNumber");
+        System.out.println(myFirstNumber);
+    }
+}
