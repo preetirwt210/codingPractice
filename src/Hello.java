57d834df137c4d25ff9e5b36d2333adfa15c96f5
Preeti Rawat <preetirawat711@gmail.com>
2021-04-03 13:56:26 +0530

    myFirst commit

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,11 @@ public class Hello {
 
         int myFirstNumber=10+6;
         System.out.println("myFirstNumber");
-        System.out.println(myFirstNumber);
+        System.out.println("My First number is: " + myFirstNumber);
+
+        int mySecondNumber=12;
+        int myThirdNumber=6;
+        int result=mySecondNumber + myThirdNumber;
+        System.out.println("Result of Addition of second and third no is : "+result);
     }
 }
